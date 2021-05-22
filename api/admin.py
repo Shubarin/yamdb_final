45c0e0144c528344cf232bda92abb2e7e1133e75
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-05-22 12:21:14 +0300

    fix imports to PEP8

--- api/admin.py
+++ api/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models.categories import Category
 from .models.comments import Comments
 from .models.genres import Genre
-from .models.users import MyUser
 from .models.review import Review
 from .models.titles import Title
+from .models.users import MyUser
 
 admin.site.register(Category)
 admin.site.register(Comments)
