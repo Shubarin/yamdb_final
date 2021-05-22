42afc307dbf0ece2737bf39ed05bdd4c5e685c55
Кирилл Шубарин <kirill.shubarin@gmail.com>
2021-05-22 12:26:22 +0300

    fixed workflow tests

--- api/views/users.py
+++ api/views/users.py
@@ -1,3 +1,4 @@
+from api_yamdb.settings import EMAIL_HOST_USER
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
@@ -10,7 +11,6 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.tokens import AccessToken
 
-from api_yamdb.settings import EMAIL_HOST_USER
 from ..permissions import IsAdmin
 from ..serializers.users import (EmailSerializer, MyUserSerializer,
                                  UserRegistrationSerializer)
